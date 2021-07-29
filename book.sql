3716015750bba58489fc9f4c1e51d184b948c91e
= <=>
2021-07-29 21:21:00 +0700

    add value

--- book.sql
+++ book.sql
@@ -41,3 +41,19 @@ CREATE TABLE comment(
     FOREIGN KEY(user_id) REFERENCES users(id)
     FOREIGN KEY(book_id) REFERENCES book(id)
 );
+
+INSERT INTO role(name) VALUES("ROLE_SUPER_ADMIN");
+INSERT INTO role(name) VALUES("ROLE_ADMIN");
+INSERT INTO role(name) VALUES("ROLE_USER");
+
+INSERT INTO users(email, password, role_id) VALUES("hvakim@gmail.com","password",3);
+INSERT INTO users(email, password, role_id) VALUES("admin@gmail.com","password",2);
+INSERT INTO users(email, password, role_id) VALUES("superadmin@gmail.com","password",1);
+
+INSERT INTO book(title, author, user_id) VALUES("Tieng Viet 1","NXBGDVDT",1);
+INSERT INTO book(title, author, user_id) VALUES("Tieng Viet 2","NXBGDVDT",2);
+INSERT INTO book(title, author, user_id) VALUES("Tieng Viet 3","NXBGDVDT",3);
+
+INSERT INTO comment(message, user_id, book_id) VALUES("Sach hay",1,1);
+INSERT INTO comment(message, user_id, book_id) VALUES("Sach hay",2,3);
+INSERT INTO comment(message, user_id, book_id) VALUES("Sach hay",3,2);
