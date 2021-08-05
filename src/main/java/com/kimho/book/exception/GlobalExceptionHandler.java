7c0b362b11cb19d8339a8ba1160eab900341f54d
= <=>
2021-08-05 20:13:40 +0700

    add respone entity exception

--- src/main/java/com/kimho/book/exception/GlobalExceptionHandler.java
+++ src/main/java/com/kimho/book/exception/GlobalExceptionHandler.java
@@ -1,30 +1,36 @@
 package com.kimho.book.exception;
 
+import com.kimho.book.model.exception.ResponseException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.lang.module.ResolutionException;
+
 @ControllerAdvice
 @RestController
 public class GlobalExceptionHandler {
     @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler(value = NotFoundException.class)
-    public String handlerNotFoundException(NotFoundException e){
-        return e.getMessage();
+    public ResponseException handlerNotFoundException(NotFoundException e){
+        ResponseException re = new ResponseException(404, "Not Found", e.getMessage());
+        return re;
     }
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(value = BadRequestException.class)
-    public String handlerBadRequestException(BadRequestException e){
-        return e.getMessage();
+    public ResponseException handlerBadRequestException(BadRequestException e){
+        ResponseException re = new ResponseException(400, "Bad Request", e.getMessage());
+        return re;
     }
 
     @ResponseStatus(HttpStatus.UNAUTHORIZED)
     @ExceptionHandler(value = UnauthorizedException.class)
-    public String handlerUnauthorizedException(UnauthorizedException e){
-        return e.getMessage();
+    public ResponseException handlerUnauthorizedException(UnauthorizedException e){
+        ResponseException re = new ResponseException(401, "Unauthorized", e.getMessage());
+        return re;
     }
 
 }
