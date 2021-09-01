aab7e10540094ff1695b3df2f8dcd13470c7b2e2
hovananhkim <hvakim.vn@gmail.com>
2021-09-01 15:00:10 +0700

    fix login google

--- src/main/java/com/kimho/book/security/oauth2/user/CustomOAuth2User.java
+++ src/main/java/com/kimho/book/security/oauth2/user/CustomOAuth2User.java
@@ -28,7 +28,7 @@ public class CustomOAuth2User implements OAuth2User {
 
     @Override
     public String getName() {
-        return oAuth2User.getName();
+        return oAuth2User.getAttribute("name");
     }
 
     public String getEmail() {
