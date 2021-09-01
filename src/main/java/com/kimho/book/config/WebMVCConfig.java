e47f510f89e4910d24fb2543506f9aff18185a29
hovananhkim <hvakim.vn@gmail.com>
2021-09-01 10:33:05 +0700

    Add cors cofig

--- src/main/java/com/kimho/book/config/WebMVCConfig.java
+++ src/main/java/com/kimho/book/config/WebMVCConfig.java
@@ -9,7 +9,7 @@ public class WebMVCConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
+                .allowedOrigins("http://localhost:3000")
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
